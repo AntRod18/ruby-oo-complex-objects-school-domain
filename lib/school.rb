a5634f3c6b230ef14efaade0608354920cc7bb22
AntRod18 <antrod1895@gmail.com>
2021-02-11 18:03:06 -0500

    commit tree bcd7733f4a7166296a274fc6c2509ea40b2ce959

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School
+attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(students, grades)
+        @roster[grades] ||= []
+        @roster[grades] << students
+        # if @roster[grades] != nil
+        #     @roster[grades]
+        # else 
+        #     @roster[grades] = []
+        # end
+        # @roster[grades] << students
+    end
+
+    def grade(grades)
+        @roster[grades]
+    end
+
+    def sort
+        organized = {}
+        @roster.each do |grades, students|
+            organized[grades] = students.sort
+        end
+        organized
+    end
+
+
+
+end
